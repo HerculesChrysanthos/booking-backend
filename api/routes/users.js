961f819b3e5c86d0cde2c11faf62ba58436d6850
Hercules Chrysanthos <h.chrysanthos>
2022-11-26 23:17:30 +0200

    Remove unnecessary line

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
 
 const emailValidation = require('../middleware/emailValidation');
 const userController = require('../controllers/users');
